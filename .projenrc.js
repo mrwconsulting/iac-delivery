cd0eb680f61eb0909cdb863c664042e9684073a8
Mark Washburn <mark@mrwconsulting.tech>
2022-05-30 13:10:12 -0500

    initial commit

--- .projenrc.js
+++ .projenrc.js
@@ -4,12 +4,17 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'mark@mrwconsulting.tech',
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
-  name: 'iac-delivery',
+  name: '@mrwconsulting/iac-delivery',
   repositoryUrl: 'https://github.com/mrwconsulting/iac-delivery.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+  python: {
+    distName: 'iac-delivery',
+    module: 'iac_delivery',
+  },
 });
+project.gitignore.addPatterns('.venv/');
 project.synth();
